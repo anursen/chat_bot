b4e3f09c90d721ab96dbbcc99685a30955441814
AN <anursen0007@gmail.com>
2024-10-27 12:52:16 -0400

    added langgraph db memopry file for db memory

---
+++ jarvis/langgraph_db_memory.py
@@ -0,0 +1,135 @@
+from IPython.display import Image, display
+from langgraph.graph import StateGraph, START
+import sqlite3
+from langgraph.checkpoint.sqlite import SqliteSaver
+
+# In memory example
+#conn = sqlite3.connect(":memory:", check_same_thread = False)
+
+db_path = "state_db/example.db"
+conn = sqlite3.connect(db_path, check_same_thread=False)
+
+# Here is our checkpointer
+memory = SqliteSaver(conn)
+
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, RemoveMessage
+
+from langgraph.graph import END
+from langgraph.graph import MessagesState
+
+model = ChatOpenAI(model="gpt-4o-mini",temperature=0)
+
+
+class State(MessagesState):
+    summary: str
+
+
+# Define the logic to call the model
+def call_model(state: State):
+    # Get summary if it exists
+    summary = state.get("summary", "")
+
+    # If there is summary, then we add it
+    if summary:
+
+        # Add summary to system message
+        system_message = f"Summary of conversation earlier: {summary}"
+
+        # Append summary to any newer messages
+        messages = [SystemMessage(content=system_message)] + state["messages"]
+
+    else:
+        messages = state["messages"]
+
+    response = model.invoke(messages)
+    return {"messages": response}
+
+
+def summarize_conversation(state: State):
+    # First, we get any existing summary
+    summary = state.get("summary", "")
+
+    # Create our summarization prompt
+    if summary:
+
+        # A summary already exists
+        summary_message = (
+            f"This is summary of the conversation to date: {summary}\n\n"
+            "Extend the summary by taking into account the new messages above:"
+        )
+
+    else:
+        summary_message = "Create a summary of the conversation above:"
+
+    # Add prompt to our history
+    messages = state["messages"] + [HumanMessage(content=summary_message)]
+    response = model.invoke(messages)
+
+    # Delete all but the 2 most recent messages
+    delete_messages = [RemoveMessage(id=m.id) for m in state["messages"][:-2]]
+    return {"summary": response.content, "messages": delete_messages}
+
+
+# Determine whether to end or summarize the conversation
+def should_continue(state: State):
+    """Return the next node to execute."""
+
+    messages = state["messages"]
+
+    # If there are more than six messages, then we summarize the conversation
+    if len(messages) > 6:
+        return "summarize_conversation"
+
+    # Otherwise we can just end
+    return END
+
+
+# Define a new graph
+workflow = StateGraph(State)
+workflow.add_node("conversation", call_model)
+workflow.add_node(summarize_conversation)
+
+# Set the entrypoint as conversation
+workflow.add_edge(START, "conversation")
+workflow.add_conditional_edges("conversation", should_continue)
+workflow.add_edge("summarize_conversation", END)
+
+# Compile
+graph = workflow.compile(checkpointer=memory)
+
+# Generate the PNG image from your graph
+png_image = graph.get_graph().draw_mermaid_png()
+# Display the image
+#display(Image(png_image))
+# Save the PNG to a file
+with open("graph_image.png", "wb") as f:
+    f.write(png_image)
+
+# Create a thread
+config = {"configurable": {"thread_id": "1"}}
+
+# Start conversation
+input_message = HumanMessage(content="hi! I'm Lance")
+output = graph.invoke({"messages": [input_message]}, config)
+for m in output['messages'][-1:]:
+    m.pretty_print()
+
+input_message = HumanMessage(content="what's my name?")
+output = graph.invoke({"messages": [input_message]}, config)
+for m in output['messages'][-1:]:
+    m.pretty_print()
+
+input_message = HumanMessage(content="i like the 49ers!")
+output = graph.invoke({"messages": [input_message]}, config)
+for m in output['messages'][-1:]:
+    m.pretty_print()
+
+config = {"configurable": {"thread_id": "1"}}
+graph_state = graph.get_state(config)
+graph_state
+
+# Create a thread
+config = {"configurable": {"thread_id": "1"}}
+graph_state = graph.get_state(config)
+graph_state
